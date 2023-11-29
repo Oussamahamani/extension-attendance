f93bf7658daef6bbfca3f8bc2b56decaeebb941f
oussamahamani <oussamahamani2000@gmail.com>
2023-11-29 18:41:07 -0500

    adding a modal and new button

--- attendance.js
+++ attendance.js
@@ -1,68 +1,248 @@
+function createModal(initialAbsences = [], initialLates = []) {
+    // Create modal elements
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
 
+    const closeButton = document.createElement('span');
+    closeButton.classList.add('close');
+    closeButton.innerHTML = '&times;';
+    closeButton.addEventListener('click', closeModal);
 
-window.addEventListener("load", async()=>{
-    console.log(document.querySelector('#student-list').children)
+    // Create section for absences
+    const absenceSection = createSection('Absences', 'absenceDate', 'absenceList', 'submitAbsence', initialAbsences);
 
+    // Create section for lates
+    const lateSection = createSection('Lates', 'lateDate', 'lateList', 'submitLate', initialLates);
 
+    // Append elements to the modal
+    modal.appendChild(closeButton);
+    modal.appendChild(absenceSection);
+    modal.appendChild(lateSection);
 
-    let users= document.querySelector('#student-list').children
-    let counter = 0
-    if(users.length<5){
-       const interval = Array.from({ length: 200 }, () => null);
-       for (let i of interval){
-        users= document.querySelector('#student-list').children
-           if(users.length>5){
-            console.log('finished',users)
-               break;
-           }
-           counter++
-           await new Promise(resolve => setTimeout(resolve, 100));     
-       }
+    // Create overlay
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    overlay.addEventListener('click', closeModal);
+
+    // Append modal and overlay to the body
+    document.body.appendChild(modal);
+    document.body.appendChild(overlay);
+
+    // Display modal and overlay
+    modal.style.display = 'flex';
+    overlay.style.display = 'block';
+  }
+
+  function createSection(title, inputId, listId, buttonId, initialData) {
+    const section = document.createElement('div');
+    section.classList.add('modal-section');
+
+    const titleLabel = document.createElement('h3');
+    titleLabel.textContent = title;
+
+    const dateInput = document.createElement('input');
+    dateInput.setAttribute('type', 'date');
+    dateInput.setAttribute('id', inputId);
+
+    const submitButton = document.createElement('button');
+    submitButton.setAttribute('id', buttonId);
+    submitButton.textContent = 'Submit';
+    submitButton.addEventListener('click', () => handleSubmit(inputId, listId));
+
+    const dateList = document.createElement('div');
+    dateList.setAttribute('id', listId);
+    dateList.classList.add('date-list');
+
+    section.appendChild(titleLabel);
+    section.appendChild(dateInput);
+    section.appendChild(submitButton);
+    section.appendChild(dateList);
+
+    // Populate initial data
+    initialData.forEach(date => {
+      const dateItem = createInitialDateItem(date, listId);
+      dateList.appendChild(dateItem);
+    });
+
+    return section;
+  }
+
+  function createInitialDateItem(date, listId) {
+    const dateItem = document.createElement('div');
+    dateItem.classList.add('date-item');
+
+    const dateText = document.createElement('span');
+    dateText.textContent = formatDate(date);
+
+    const deleteButton = document.createElement('span');
+    deleteButton.classList.add('delete-button');
+    deleteButton.innerHTML = '&times;';
+    deleteButton.addEventListener('click', () => deleteDate(dateItem, listId, date));
+
+    dateItem.appendChild(dateText);
+    dateItem.appendChild(deleteButton);
+
+    return dateItem;
+  }
+
+  function closeModal() {
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+
+    // Remove modal and overlay from the body
+    document.body.removeChild(modal);
+    document.body.removeChild(overlay);
+  }
+
+  function handleSubmit(inputId, listId) {
+    const dateInput = document.getElementById(inputId);
+    const dateList = document.getElementById(listId);
+    const dateValue = dateInput.value;
+
+    if (dateValue) {
+      const dateItem = createInitialDateItem(dateValue, listId);
+      dateList.appendChild(dateItem);
+
+      // Clear the date input
+      dateInput.value = '';
     }
-    console.log('last',users)
-    for(let user of users){
-        const email = document.createElement("button");
-        email.style.position = "absolute";
-        email.style.top = "0px";
-        email.style.right = "55px";
-        email.style.height = "25px";
-        email.style.width = "100px";
-        email.innerText = "absent email";
-        email.style.zIndex = 100;
-        const late = document.createElement("button");
-        late.style.position = "absolute";
-        late.style.top = "25px";
-        late.style.right = "55px";
-        late.style.height = "25px";
-        late.style.width = "100px";
-        late.innerText = "late email";
-        late.style.zIndex = 100;
-        let name = user.querySelector(".student-name").innerText
-        email.addEventListener('click',()=>{
-            console.log(name)
-            // navigator.clipboard.writeText(name)
-            chrome.storage.sync.get([
-             name
-            ],(res)=>{
-         console.log(res)
-         let data = res[name]
-        let absences = data.filter((item)=>item.status==="absent").sort((a, b) => a.timeStamp + b.timeStamp)
-            
-            
+  }
 
+  function deleteDate(dateItem, listId, date) {
+    const dateList = document.getElementById(listId);
+    dateList.removeChild(dateItem);
+
+    // You may want to handle additional logic when deleting the date
+    console.log(`Date ${date} deleted from ${listId}`);
+  }
+
+  function formatDate(dateString) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+  }
+
+
+
+
+
+  
+window.addEventListener(
+  "load",
+  async () => {
+    console.log(document.querySelector("#student-list").children);
+
+    let users = document.querySelector("#student-list").children;
+    let counter = 0;
+    if (users.length < 5) {
+      const interval = Array.from({ length: 200 }, () => null);
+      for (let i of interval) {
+        users = document.querySelector("#student-list").children;
+        if (users.length > 5) {
+          console.log("finished", users);
+          break;
+        }
+        counter++;
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
+    console.log("last", users);
+    for (let user of users) {
+      const email = document.createElement("button");
+      email.style.position = "absolute";
+      email.style.top = "0px";
+      email.style.right = "55px";
+      email.style.height = "25px";
+      email.style.width = "100px";
+      email.innerText = "absent email";
+      email.style.zIndex = 100;
+      const late = document.createElement("button");
+      late.style.position = "absolute";
+      late.style.top = "25px";
+      late.style.right = "55px";
+      late.style.height = "25px";
+      late.style.width = "100px";
+      late.innerText = "late email";
+      late.style.zIndex = 100;
+
+      const Edit = document.createElement("button");
+      Edit.style.position = "absolute";
+      Edit.style.top = "0px";
+      Edit.style.right = "155px";
+      Edit.style.height = "50px";
+      Edit.style.width = "100px";
+      Edit.innerText = "Edit";
+      Edit.style.zIndex = 100;
+        
+      let name = user.querySelector(".student-name").innerText;
+
+      Edit.addEventListener('click',()=>{
+        console.log('hi')
+        chrome.storage.sync.get([name, "settings"], (res) => {
+
+            let data = res[name] || [];
+            let absences = data.filter((item)=>item.status ==="absent").map((item)=>item.date)
+            let lates = data.filter((item)=>item.status ==="late").map((item)=>item.date)
+            console.log(absences,lates)
+            createModal(absences,lates)
+        })
+      })
+
+      email.addEventListener("click", () => {
+        console.log(name);
+        chrome.storage.sync.get([name, "settings"], (res) => {
+          console.log(res);
+          if (!res.settings){
          
-            })
-        })
-        late.addEventListener('click',()=>{
+            return
+          }
+          let data = res[name];
+          let { cycle, support, instructor } = res.settings;
+          let absences = data
+            .filter((item) => item.status === "absent")
+            .sort((a, b) => a.timeStamp + b.timeStamp);
 
-        })
-        user.appendChild(email);
-        user.appendChild(late);
+          let left = 2;
+          let occurences = "1st";
+          if (absences.length === 2) {
+            left = 1;
+            occurences = "2nd";
+          } else if (absences.length === 3) {
+            left = 0;
+            occurences = "3rd";
+          } else if (absences.length > 3) {
+            left = 0;
+            occurences = absences.length + "th";
+          }
+
+          let str = `Hello ${name.split(" ")[0]}, 
+This email is to inform you that you have recorded your ${occurences} absence in the course ${cycle}.
+
+You have been absent:
+${absences.map((item) => item.formatedDate).join("\n")}
+
+Per your enrollment agreement, you only have ${left} absence${
+            left > 1 ? "s" : ""
+          } left at your disposal. 
+
+We would like to work with you to help remediate any blockers that are presenting themselves and preventing you from attending class.
+
+Please feel free to reach out to your instructor: 
+${instructor}
+Or 
+${support}`;
+console.log(str)
+        navigator.clipboard.writeText(str)
+
+        });
+      });
+      late.addEventListener("click", () => {});
+      user.appendChild(email);
+      user.appendChild(late);
+      user.appendChild(Edit)
     }
+  },
+  false
+);
 
 
-
-
-
-
-}, false);
--- background.js
+++ background.js
@@ -1,45 +1,11 @@
-// //runs when extension is installed or you can add some stuff that are general like adding option to right click menu or when seleceting text
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason === "install") {
+    // Open the options page on installation
+    chrome.runtime.openOptionsPage();
+  }
+});
 
-// // console.log('hellor from background script')
 
-// // chrome.tabs.executeScript(tabOfInterestId,{
-// //     frameId: "tool_content",
-// //     file: "./contentScript.js"
-// // },function(results){
-// //     console.log('yay')
-// //     //Handle any results
-// // });
-
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     if (request.action === 'performDOMOperations') {
-//       // Trigger DOM operations
-//       console.log('Background DOM operations');
-
-//       chrome.tabs.create({ url: 'https://perscholas.instructure.com/courses/1671/users',active: false }, function(newTab) {
-//     // Tab is created
-
-//       })
-//     }
-//   });
-
-// //   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-// //     if (changeInfo.status == 'complete' && tab.url.includes('https://perscholas.instructure.com/courses/1671/external_tools/12')) {
-// //         chrome.tabs.executeScript(tabId, {
-// //             code: 'document.body.innerHTML;'
-// //         }, function(results) {
-// //             // Now you have access to the DOM of the page
-// //             console.log(results[0]); // Outputs the innerHTML of the body element.
-// //         });
-// //     }
-// // });
-
-// chrome.runtime.onInstalled.addListener((details)=>{
-//     chrome.contextMenus.create({
-//         title:"copy email",
-//         id:"emailMenu",
-//         contexts:["all"]
-//     })
-// })
 function getOrdinalSuffix(day) {
   if (day > 3 && day < 21) return 'th'; // thanks to exception rules
   switch (day % 10) {
