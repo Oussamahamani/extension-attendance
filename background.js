50eaedcdaa5ff1e7a4131030478b5ac84bf661a0
oussamahamani <oussamahamani2000@gmail.com>
2023-11-28 21:12:03 -0500

    tried multiple solution, opening new tab to get all the students

--- background.js
+++ background.js
@@ -1,3 +1,44 @@
-//runs when extension is installed or you can add some stuff that are general like adding option to right click menu or when seleceting text
+// //runs when extension is installed or you can add some stuff that are general like adding option to right click menu or when seleceting text
 
-console.log('hellor from background script')
+// // console.log('hellor from background script')
+
+// // chrome.tabs.executeScript(tabOfInterestId,{
+// //     frameId: "tool_content",
+// //     file: "./contentScript.js"
+// // },function(results){
+// //     console.log('yay')
+// //     //Handle any results
+// // });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === 'performDOMOperations') {
+      // Trigger DOM operations
+      console.log('Background DOM operations');
+
+      chrome.tabs.create({ url: 'https://perscholas.instructure.com/courses/1671/users',active: false }, function(newTab) {
+    // Tab is created
+
+      })
+    }
+  });
+
+
+// //   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// //     if (changeInfo.status == 'complete' && tab.url.includes('https://perscholas.instructure.com/courses/1671/external_tools/12')) {
+// //         chrome.tabs.executeScript(tabId, {
+// //             code: 'document.body.innerHTML;'
+// //         }, function(results) {
+// //             // Now you have access to the DOM of the page
+// //             console.log(results[0]); // Outputs the innerHTML of the body element.
+// //         });
+// //     }
+// // });
+
+
+chrome.runtime.onInstalled.addListener((details)=>{
+    chrome.contextMenus.create({
+        title:"copy email",
+        id:"emailMenu",
+        contexts:["all"]
+    })
+})
--- contentScript.js
+++ contentScript.js
@@ -1,2 +1,50 @@
 //runs on specfic page
-console.log("hello from content script")
+
+window.addEventListener("load", myMain, false);
+
+
+function openNewTab(url) {
+    chrome.runtime.sendMessage({ action: 'performDOMOperations' });
+    console.log("hellor world", "running");
+    // chrome.tabs.create({ url: url, active: false })
+    //     console.log("hellor world", "created");
+  }
+  
+  // Function to perform DOM operations
+  function performDOMOperations(tabId) {
+    // Use chrome.tabs.executeScript to run scripts in the context of the new tab
+    chrome.tabs.executeScript(tabId, { code: 'console.log("DOM operations in the new tab");' });
+  }
+
+
+async function myMain(evt) {
+ openNewTab('https://perscholas.instructure.com/courses/1671/users');
+  try {
+    // let l = document.querySelector("#student-list li");
+    // console.log("hellor world", l);
+    // var newElement = document.createElement("a");
+    // newElement.style.position = "absolute";
+    // newElement.style.top = "0";
+    // newElement.style.right = "0";
+    // newElement.style.width = 50;
+    // newElement.style.height = 20;
+    // newElement.style.innerText = "copy";
+    // newElement.style.zIndex = 10;
+    // l.appendChild(newElement);
+        // Select the iframe element by its ID
+    //     var iframe = document.getElementById('tool_content');
+    //     console.log("hellor world","frame",iframe);
+    //     var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+    //     // Wait until the iframe has loaded its content
+    //   let l=  iframeDoc.querySelector("#student-list li")
+            
+    //            // let l = document.querySelector("#student-list li");
+    // console.log("hellor world", l);
+     
+
+ 
+  } catch (err) {
+    console.log("hellor world", err);
+  }
+}
